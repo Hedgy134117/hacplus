5559a8bdd9c20a8e69759369df12a1b01f746c9d
Hedgy134117 <hedgy134117@gmail.com>
2021-01-05 14:25:33 -0500

    finish basic functionality

--- content.js
+++ content.js
@@ -1,73 +1,73 @@
-let colors = {
-    "settings__color--red": "#FF9AA2",
-    "settings__color--salmon": "#FFB7B2",
-    "settings__color--orange": "#FFDAC1",
-    "settings__color--yellow": "#E2F0CB",
-    "settings__color--green": "#B5EAD7",
-    "settings__color--purple": "#C7CEEA"
-};
+class classFunctions {
+    static colors = {
+        "settings__color--red": "#FF9AA2",
+        "settings__color--salmon": "#FFB7B2",
+        "settings__color--orange": "#FFDAC1",
+        "settings__color--yellow": "#E2F0CB",
+        "settings__color--green": "#B5EAD7",
+        "settings__color--purple": "#C7CEEA"
+    };
 
-const cleanClassName = (name) => {
-    return name.toLowerCase().split(' ').join('')
-};
+    static cleanClassName = (name) => {
+        return name.split(' ')[0].toLowerCase()
+    };
 
-const colorClass = (_class) => {
-    _class.style.backgroundColor = "lightgray";
-    let color = localStorage.getItem(cleanClassName(_class.children[0].innerText) + 'Color');
-    if (color != null) {
-        _class.style.backgroundColor = color;
-    }
-}
+    static colorClass = (_class) => {
+        _class.style.backgroundColor = "lightgray";
+        let color = localStorage.getItem(this.cleanClassName(_class.children[0].innerText) + 'Color');
+        if (color != null) {
+            _class.style.backgroundColor = color;
+        }
+    };
 
-const classBehaviour = (_class) => {
-    _class.addEventListener("click", (e) => {
-        // When clicking on it, update the name of the grades section
-        let name = e.target.children[0].innerText;
-        document
-            .getElementsByClassName("grades")[0]
-            .getElementsByClassName("grades__label")[0].innerText = name;
-
-        // Update the grades section to the class selected
-        let grades = document.getElementsByClassName('grades__list');
-        for (let j = 0; j < grades.length; j++) {
-            if (grades[j].id == cleanClassName(name) + 'Grades') {
-                if (grades[j].classList.contains('hidden')) {
-                    grades[j].classList.remove('hidden');
+    static classBehaviour = (_class) => {
+        _class.addEventListener("click", (e) => {
+            // When clicking on it, update the name of the grades section
+            let name = e.target.children[0].innerText;
+            document
+                .getElementsByClassName("grades")[0]
+                .getElementsByClassName("grades__label")[0].innerText = name;
+    
+            // Update the grades section to the class selected
+            let grades = document.getElementsByClassName('grades__list');
+            for (let j = 0; j < grades.length; j++) {
+                if (grades[j].id == this.cleanClassName(name) + 'Grades') {
+                    if (grades[j].classList.contains('hidden')) {
+                        grades[j].classList.remove('hidden');
+                    }
+                }
+                else {
+                    if (!(grades[j].classList.contains('hidden'))) {
+                        grades[j].classList.add('hidden');
+                    }
                 }
             }
-            else {
-                if (!(grades[j].classList.contains('hidden'))) {
-                    grades[j].classList.add('hidden');
-                }
+        });
+    };
+
+    static settingsBehaviour = (setting) => {
+        setting.addEventListener("click", (e) => {
+            let parent = e.target.parentElement;
+            let settings = parent.getElementsByClassName("class__settings")[0];
+    
+            if (settings.classList.contains("hidden")) {
+                settings.classList.remove("hidden");
+            } else {
+                settings.classList.add("hidden");
             }
-        }
-    });
+        });
+    };
+
+    static colorsBehaviour = (color) => {
+        color.addEventListener("click", (e) => {
+            let parent = e.target.parentElement.parentElement.parentElement;
+            parent.style.backgroundColor = colors[e.target.classList[1]];
+    
+            let name = parent.children[0].innerText;
+            localStorage.setItem(this.cleanClassName(name) + 'Color', colors[e.target.classList[1]]);
+        });
+    };
 }
-
-const settingsBehaviour = (setting) => {
-    setting.addEventListener("click", (e) => {
-        let parent = e.target.parentElement;
-        let settings = parent.getElementsByClassName("class__settings")[0];
-
-        if (settings.classList.contains("hidden")) {
-            settings.classList.remove("hidden");
-        } else {
-            settings.classList.add("hidden");
-        }
-    });
-}
-
-const colorsBehaviour = (color) => {
-    color.addEventListener("click", (e) => {
-        let parent = e.target.parentElement.parentElement.parentElement;
-        parent.style.backgroundColor = colors[e.target.classList[1]];
-
-        let name = parent.children[0].innerText;
-        localStorage.setItem(cleanClassName(name) + 'Color', colors[e.target.classList[1]]);
-    });
-}
-
-
 
 const main = () => {
     let loc = new URL(window.location.href);
@@ -144,32 +144,58 @@ const classwork = () => {
             </div>
         `;
 
+        // Classes Boxes
         for (let c in data) {
             cont.querySelector('.classes__classes').innerHTML += `
           <div class="classes__class">
             <p class="class__name">${c}</p>
             <p class="class__percent">${data[c]['grade']}%</p>
-            <p class="class__settingsIcon fa fa-gear"></p>
-            <div class="class__settings hidden">
-              <p class="settings__label">Settings for ${c}</p>
-              <div>
-                <div class="settings__color settings__color--red"></div>
-                <div class="settings__color settings__color--salmon"></div>
-                <div class="settings__color settings__color--orange"></div>
-                <div class="settings__color settings__color--yellow"></div>
-                <div class="settings__color settings__color--green"></div>
-                <div class="settings__color settings__color--purple"></div>
-              </div>
-            </div>
           </div>
             `;
         }
+        // Add the proper functionality to the boxes
         let classesContainers = cont.querySelectorAll('.classes__class');
-        console.log(classesContainers);
         for (let i = 0; i < classesContainers.length; i++) {
-            colorClass(classesContainers[i]);
-            classBehaviour(classesContainers[i]);
+            classFunctions.colorClass(classesContainers[i]);
+            classFunctions.classBehaviour(classesContainers[i]);
         }
+
+        // Set up grades
+        cont.querySelector('.grades').innerHTML += `
+            <p class="grades__label">Grades</p>        
+        `;
+
+        // Grades Sections
+        for (let c in data) {
+            let id = classFunctions.cleanClassName(c) + 'Grades';
+            cont.querySelector('.grades').innerHTML += `
+                <div class="grades__list hidden" id="${id}">
+                    <div class="list__grade">
+                        <p class="grade__name"><b>Assignment Name</b></p>
+                        <p class="grade__points"><b>Points</b></p>
+                        <p class="grade__totalPoints"><b>Total Points</b></p>
+                        <p class="grade__percent"><b>Percentage</b></p>
+                    </div>
+                </div>
+            `;
+
+            for (let assignment in data[c]) {
+                if (assignment != 'grade') {
+                    let points = data[c][assignment]['points'];
+                    let totalPoints = data[c][assignment]['totalPoints'];
+                    cont.querySelector(`#${id}`).innerHTML += `
+                    <div class="list__grade">
+                        <p class="grade__name">${assignment}</p>
+                        <p class="grade__points">${points}</p>
+                        <p class="grade__totalPoints">${totalPoints}</p>
+                        <p class="grade__percent">${((points / totalPoints) * 100).toFixed(2)}%</p>
+                    </div>
+                    `;
+                }
+            }
+        }
+
+
     };
 
     loadNewView(getData());
